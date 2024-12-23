60ee5c48c821ac87de12a8d65d317662cd2dbdb1
deep-coder1 <ndktbd@gmail.com>
2024-12-23 18:30:52 +0530

    Create ngmodule app first commit

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Angular Tut Code with Technyks
 
 <!-- Timestamps: -->
 <!-- 41:26 - NgModule vs Standalone -->
+<!-- 42:22 - Create Angular ngModule Project -->
---
+++ first-app-ngmodule/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: '<div>Coding World</div>',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'first-app-ngmodule';
+}
